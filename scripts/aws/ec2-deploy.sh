ed7090fbdb4e1329a69efdde4e592fc8030d6c56
Matt Pillar <matt@aerofs.com>
2014-10-22 11:01:05 -0700

    Small usability improvements to ec2 deploy script.

--- scripts/aws/ec2-deploy.sh
+++ scripts/aws/ec2-deploy.sh
@@ -2,11 +2,11 @@
 # Launch your AeroFS Appliance (.qcow.gz format) in EC2.
 # We assume your access key and secret are in the enviroment.
 
-set -eux
+set -eu
 
 if [ $# -ne 3 ]
 then
-    echo "Usage: $0 <security_group> <s3_bucket> <qcow_gz>"
+    echo "Usage: $(basename $0) <security_group> <s3_bucket> <qcow_gz>"
     exit 0
 fi
 
@@ -14,11 +14,14 @@ security_group=$1
 s3_bucket=$2
 qcow_gz=$3
 
-gunzip $qcow_gz
-
 qcow=$(echo $qcow_gz | awk -F '.gz' '{print $1}')
+
+echo ">> Decompressing image..."
+zcat $qcow_gz > $qcow
+echo ">> Converting to RAW format..."
 qemu-img convert -O raw $qcow aerofs-appliance.raw
 
+echo ">> Importing instance..."
 ec2-import-instance \
     -t m3.xlarge \
     -f RAW \
@@ -28,4 +31,5 @@ ec2-import-instance \
     -g $security_group \
     -b $s3_bucket aerofs-appliance.raw
 
+echo ">> Instance will be available after conversion:"
 ec2-describe-conversion-tasks
